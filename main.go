a0c78e8ffa626590daf8b978b22a78887a97c853
Wang Cong <ousatoru@hotmail.com>
2017-04-10 17:31:26 +0800

    move out try form

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
-const replyPage = "web/tmpl/reply.html"
+const replyPage = "web/reply.html"
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
@@ -15,7 +16,13 @@ func main() {
 
 func reply(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		t, _ := template.ParseFiles(replyPage)
-		t.Execute(w, nil)
+		if r.Method == "GET" {
+			t, _ := template.ParseFiles(replyPage)
+			t.Execute(w, nil)
+		} else {
+			r.ParseForm()
+			fmt.Println(r.Form)
+			fmt.Fprintf(w, "age: %v", r.Form["age"])
+		}
 	}
 }
