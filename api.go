e0cf84e1e0a6fa8f4360aa854f6e69c09c28e581
Wang Cong <ousatoru@hotmail.com>
2017-04-14 15:27:53 +0800

    ittan

--- api.go
+++ api.go
@@ -1,27 +1,58 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+)
 
 type Reply struct {
-	ReplyNo       string   `json:"reply_no"`
-	ReplyType     string   `json:"reply_type"`
-	ReplyYear     string   `json:"reply_year"`
-	Borrower      string   `json:"borrower"`
-	IDNo          string   `json:"id_no"`
-	TotalAmount   int      `json:"total_amount"`
-	Head          string   `json:"head"`
-	Comprehensive Credit   `json:"comprehensive"`
-	Interim       []Credit `json:"interim"`
+	ReplyNo       string     `json:"reply_no"`
+	ReplyType     string     `json:"reply_type"`
+	ReplyYear     string     `json:"reply_year"`
+	Borrower      string     `json:"borrower"`
+	IDNo          string     `json:"id_no"`
+	TotalAmount   int        `json:"total_amount"`
+	Head          string     `json:"head"`
+	Comprehensive Security   `json:"comprehensive"`
+	Interim       []Security `json:"interim"`
 }
 
-type Comprehensive struct {
-	CreditKind string `json:"credit_kind"`
+type Security struct {
+	Mortgage  []Mortgage  `json:"mortgage"`
+	Pledge    []Pledge    `json:"pledge"`
+	Guarantee []Guarantee `json:"guarantee"`
+	Credit    []Credit    `json:"credit"`
 }
 
-type Interim struct {
+type Mortgage struct {
+	Loan
+	Collateral string `json:"collateral"`
+	Owner      string `json:"owner"`
+	Position   string `json:"position"`
+	Area       string `json:"area"`
+	Value      string `json:"value"`
+}
+
+type Pledge struct {
+	Loan
+	Material string `json:"material"`
+	Owner    string `json:"owner"`
+	Value    string `json:"value"`
+}
+
+type Guarantee struct {
+	Loan
+	GuarantorKind string `json:"guarantor_kind"`
+	Guarantor     string `json:"guarantor"`
+	IDNo          string `json:"id_no"`
 }
 
 type Credit struct {
+	Loan
+}
+
+type Loan struct {
+	Category     string `json:"category"`
 	BeginDate    string `json:"begin_date"`
 	EndDate      string `json:"end_date"`
 	Span         int    `json:"span"`
@@ -29,6 +60,13 @@ type Credit struct {
 	DebtKind     string `json:"debt_kind"`
 	Warranty     string `json:"warranty"`
 	InterestRate string `json:"interest_rate"`
+	Limitation   string `json:"limitation"`
+	RiskTip      string `json:"risk_tip"`
+	Notes        string `json:"notes"`
+	Branch       string `json:"branch"`
+	Responsible  string `json:"responsible"`
+	Operator     string `json:"operator"`
+	Time         string `json:"time"`
 }
 
 // get::  para:: rpno= deleted=0/1 idno= compname=
@@ -39,6 +77,9 @@ func api(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 	} else if r.Method == "POST" {
 		r.ParseForm()
+		fmt.Println(r.Form)
+		fmt.Fprintf(w, "age: %v", r.Form["pfbh"])
+
 	}
 }
 
